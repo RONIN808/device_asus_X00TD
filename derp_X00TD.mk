09ae77a33c00f77a0e7d95a6acd34c886d703020
RONIN808 <shubhamshedage808@gmail.com>
2022-06-05 14:55:51 +0000

    hmm

--- derp_X00TD.mk
+++ derp_X00TD.mk
@@ -20,6 +20,9 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Official-ify
 DERP_BUILDTYPE := Official
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+TARGET_USES_BLUR := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := derp_X00TD
@@ -33,7 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 12 SP2A.220505.002 8353555 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:9/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SP2A.220505.002/8353555:user/release-keys"
