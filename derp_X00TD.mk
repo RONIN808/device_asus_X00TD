aa40bbd528934de8c36fe4f384b5c020af0ce2e8
RONIN <71311970+RONIN808@users.noreply.github.com>
2023-12-19 16:55:07 +0000

    X00TD: Update fingerprint/build description from UQ1A.231205.015

--- derp_X00TD.mk
+++ derp_X00TD.mk
@@ -33,7 +33,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230605.012 10204971 release-keys"
+    PRIVATE_BUILD_DESC="cheetah-user 14 UQ1A.231205.015 11084887 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/cheetah/cheetah:13/TQ3A.230605.012/10204971:user/release-keys"
+BUILD_FINGERPRINT := "google/cheetah/cheetah:14/UQ1A.231205.015/11084887:user/release-keys"
